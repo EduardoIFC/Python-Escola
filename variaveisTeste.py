0c4e2c4f22aca528708c8b3a087d705edbc6b2a1
Eduardo IFC <Eduardoreward123@gmail.com>
2025-05-27 13:31:27 +0000

    mostrando o tipo das variaveis

--- variaveisTeste.py
+++ variaveisTeste.py
@@ -2,3 +2,8 @@ aluno = "Ada Lovelace"
 periodo_corrente = 10
 ira = 9.99 #Valor que defina o IRA
 laurea = True
+
+print(type(aluno))
+print(type(periodo_corrente))
+print(type(ira))
+print(type(laurea))
