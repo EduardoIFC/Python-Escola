764d1b8bf7e8c4d598b4bcb6e8b4163f1385203a
Eduardo IFC <Eduardoreward123@gmail.com>
2025-05-30 13:25:38 +0000

    adicionado exemplos de f-strings e area passados pelo professor atrvez do livro (sigaa)

---
+++ Testes/fStrings.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+ano_atual = datetime.now().year
+clube = "SPFC"
+campeonatoMundia = 3
+anoFundado = 1930
+print(f"{clube} possui {campeonatoMundia} títulos mundiais.")
+print(F"São {ano_atual - anoFundado} anos de existência.")
